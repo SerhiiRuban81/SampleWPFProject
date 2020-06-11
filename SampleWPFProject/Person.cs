cb1b5b52fb31e2f4f0905712605b7ebe5ec86d41
tetiana.ruban@hotmail.com <ruban_sa@i.ua>
2020-06-11 17:22:26 +0300

    Класс Person добавлен

---
+++ SampleWPFProject/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWPFProject
+{
+    public class Person
+    {
+        public string Surname { get; set; }
+
+        public int Id { get; set; }
+
+        public int Age { get; set; }
+    }
+}
